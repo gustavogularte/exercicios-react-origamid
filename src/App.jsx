d6105b00567bb7a28b522af195f1655bdd760f55
Gustavo Rodrigo Gularte Arend <gustavorrarend@gmail.com>
2023-11-19 17:24:35 -0300

    Exercicio 1 jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Ex1 from './exercicios/react-para-iniciantes/jsx2';
+
+const App = () => {
+  return (
+    <main>
+      <Ex1 />
+    </main>
+  );
+};
+
+export default App;
