32f15df3fc73251f9368599fb6150eac4d02386d
Gustavo Rodrigo Gularte Arend <gustavorrarend@gmail.com>
2023-11-19 20:00:08 -0300

    ex 2 jsx arrays

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Ex1 from './exercicios/react-para-iniciantes/jsx2';
+import Ex2 from './exercicios/react-para-iniciantes/JsxArrays';
 
 const App = () => {
   return (
     <main>
       <Ex1 />
+      <Ex2 />
     </main>
   );
 };
